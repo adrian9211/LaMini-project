b3b59bb337274de8351ce97f4e2423acb78601b0
Adria Nykiel <adrian.nykiel@gmail.com>
2024-03-13 19:35:10 +0000

    beta version, LaMini-Flan-T5-248M data model fully functional, next step is to indroduce other data models

--- app.py
+++ app.py
@@ -1,58 +1,17 @@
 import base64
 import streamlit as st
 import torch
+import streamlit_shadcn_ui as ui
 from pptx import Presentation
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.document_loaders import PyPDFLoader, UnstructuredPowerPointLoader
 from transformers import T5ForConditionalGeneration
 from transformers import T5Tokenizer
 from transformers import pipeline
-from pdf2image import convert_from_path
-import os
-from transformers import PegasusTokenizer, PegasusForConditionalGeneration
-from sentence_transformers import SentenceTransformer, util
+
 
 # MODEL and TOKENIZER
 checkpoint = "LaMini-Flan-T5-248M"
 tokenizer = T5Tokenizer.from_pretrained(checkpoint)
 base_model = T5ForConditionalGeneration.from_pretrained(checkpoint, device_map='auto', torch_dtype=torch.float32)
-
-
-# Function to convert PPTX or PPT to PDF using unoconv
-def convert_ppt_to_pdf(input_file_path, output_file_prefix):
-  output_file_path = f"{output_file_prefix}.pdf"
-  command = f"unoconv -f pdf -o {output_file_path} {input_file_path}"
-  os.system(command)
-  return output_file_path
-
-
-# Display function for PDF using images
-def display_pdf(file_path):
-  images = convert_from_path(file_path)
-  for image in images:
-    st.image(image)
-
-
-st.title("PPT/PPTX Viewer")
-
-uploaded_file = st.file_uploader("Choose a PPT or PPTX file", type=["ppt", "pptx"])
-if uploaded_file is not None:
-  # Save uploaded file to disk with appropriate extension
-  file_extension = uploaded_file.name.split('.')[-1]
-  temp_file_path = f"temp_presentation.{file_extension}"
-  with open(temp_file_path, "wb") as f:
-    f.write(uploaded_file.getbuffer())
-
-  # Convert PPT or PPTX to PDF
-  pdf_path = convert_ppt_to_pdf(temp_file_path, "output_pdf")
-
-  # Display the PDF
-  display_pdf(pdf_path)
-
-  # Cleanup
-  os.remove(temp_file_path)
-  os.remove(pdf_path)
-
 
 #File Loader and Preprocessing
 def file_processing(file):
@@ -82,8 +41,8 @@ def LLM_Pipeline(filepath):
     max_length = 500,
     min_length = 25
   )
-  input_pdf = file_processing(filepath)
-  result = pipeline_summarizer(input_pdf)
+  input_pptx = file_processing(filepath)
+  result = pipeline_summarizer(input_pptx)
   result = result[0]['summary_text']
   return result
 
@@ -104,26 +63,21 @@ def displayPPTX(file):
 # st.set_page_config(layout='wide', page_title="PPTX Summarizer")
 
 def main():
-  st.title('Online PPTX Summarizer')
+  st.title('Legal-Pythia')
+  st.subheader('Online PPTX Summarizer')
   uploaded_file = st.file_uploader("Upload your PPTX File", type=['pptx'])
 
   if uploaded_file is not None:
     if st.button("Summarize"):
-      col1, col2 = st.columns(2)
+
       filepath = "data/"+uploaded_file.name
       with open(filepath, 'wb') as temp_file:
         temp_file.write(uploaded_file.read())
 
-      with col1:
-        st.info("Uploaded PDF File")
-        pdf_viewer = displayPPTX(filepath)
 
-
-      with col2:
         st.info("Here is your PDF Summarization")
         summary = LLM_Pipeline(filepath)
         st.success(summary)
-
 
 if __name__ == "__main__":
   main()
